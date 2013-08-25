28a42000bdb0c58e18a38d0139ae0e2942a94b2c
Kyle VanderBeek <kylev@kylev.com>
2013-08-25 15:27:31 -0700

    Instrument parts of AWS::Core::Client as a fallback

--- lib/newrelic_aws/instrumentation.rb
+++ lib/newrelic_aws/instrumentation.rb
@@ -14,6 +14,14 @@ DependencyDetection.defer do
   end
 
   executes do
+    # We instrument a few AWS::Core::Client methods so we aren't blind to calls that aren't
+    # yet specifically instrumented at a higher level.
+    AWS::Core::Client.class_eval do
+      add_method_tracer :client_request
+      add_method_tracer :make_async_request
+      add_method_tracer :make_sync_request
+    end
+
     AWS::S3::Bucket.class_eval do
       add_method_tracer :clear!
       add_method_tracer :delete
