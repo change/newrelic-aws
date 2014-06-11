70da5d99bc82a425d2762bb2235f84887e50991c
Kyle VanderBeek <kyle@change.org>
2014-06-11 13:33:27 -0700

    Mix in NewRelic::Agent::MethodTracer to quell warning messages.

--- lib/newrelic-aws/instrumentation.rb
+++ lib/newrelic-aws/instrumentation.rb
@@ -17,12 +17,14 @@ DependencyDetection.defer do
     # We instrument a few AWS::Core::Client methods so we aren't blind to calls that aren't
     # yet specifically instrumented at a higher level.
     AWS::Core::Client.class_eval do
+      include NewRelic::Agent::MethodTracer
       add_method_tracer :client_request
       add_method_tracer :make_async_request
       add_method_tracer :make_sync_request
     end
 
     AWS::S3::Bucket.class_eval do
+      include NewRelic::Agent::MethodTracer
       add_method_tracer :clear!
       add_method_tracer :delete
       add_method_tracer :delete!
@@ -31,13 +33,18 @@ DependencyDetection.defer do
     end
 
     AWS::S3::S3Object.class_eval do
+      include NewRelic::Agent::MethodTracer
       add_method_tracer :read
       add_method_tracer :write
       add_method_tracer :delete
       add_method_tracer :head
+
+      add_method_tracer :url_for
+      add_method_tracer :public_url
     end
 
     AWS::SQS::Queue.class_eval do
+      include NewRelic::Agent::MethodTracer
       add_method_tracer :batch_delete
       add_method_tracer :batch_send
       add_method_tracer :exists?
@@ -47,6 +54,7 @@ DependencyDetection.defer do
     end
 
     AWS::SimpleEmailService.class_eval do
+      include NewRelic::Agent::MethodTracer
       add_method_tracer :send_email
       add_method_tracer :send_raw_email
     end
